dd206c9c202ebba651a495fb834a17a457f184eb
Lewis Thorley <lewis@beacon.co.uk>
2022-07-16 17:44:35 +0100

    Styles

---
+++ pages/quiz/index.tsx
@@ -0,0 +1,9 @@
+const Quiz = () => {
+  return (
+    <div className="bg-purple-200 h-screen flex flex-col items-center justify-center">
+      Quiz here
+    </div>
+  );
+};
+
+export default Quiz;
