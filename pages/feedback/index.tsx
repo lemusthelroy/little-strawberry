5696a0899ed5c3e20f3f049b719f5c90c000882b
Lewis Thorley <lewis.thorley@netlify.com>
2022-08-04 13:03:11 +0100

    State of form

--- pages/feedback/index.tsx
+++ pages/feedback/index.tsx
@@ -2,24 +2,40 @@ import Button from "@components/Button";
 import FullScreenWrapper from "@components/FullScreenWrapper";
 import Input from "@components/Input";
 import TextArea from "@components/TextArea";
+import { useState } from "react";
 
-const Feedback = () => (
-  <FullScreenWrapper>
-    <form name="contact" method="POST" data-netlify="true">
-      <div className="flex flex-col">
-        <label htmlFor="name">Your Name:</label>
-        <Input type="text" name="name" id="name" />
+const Feedback = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-        <label htmlFor="email">Your Email:</label>
-        <Input type="text" name="email" id="email" />
+  return (
+    <FullScreenWrapper>
+      {isSubmitted ? (
+        <strong>Thanks for your feedback</strong>
+      ) : (
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          onSubmit={() => {
+            setIsSubmitted(true);
+          }}
+        >
+          <div className="flex flex-col">
+            <label htmlFor="name">Your Name:</label>
+            <Input type="text" name="name" id="name" />
 
-        <label htmlFor="message">Feedback:</label>
-        <TextArea name="message" id="message"></TextArea>
+            <label htmlFor="email">Your Email:</label>
+            <Input type="text" name="email" id="email" />
 
-        <Button type="submit">Send</Button>
-      </div>
-    </form>
-  </FullScreenWrapper>
-);
+            <label htmlFor="message">Feedback:</label>
+            <TextArea name="message" id="message"></TextArea>
+
+            <Button type="submit">Send</Button>
+          </div>
+        </form>
+      )}
+    </FullScreenWrapper>
+  );
+};
 
 export default Feedback;
