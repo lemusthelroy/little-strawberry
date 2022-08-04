15b88091c73b91624af44f35ebf16bd2778068c9
Lewis Thorley <lewis.thorley@netlify.com>
2022-08-04 13:19:43 +0100

    Try again

--- pages/feedback/index.tsx
+++ pages/feedback/index.tsx
@@ -10,31 +10,31 @@ const Feedback = () => {
   return (
     <FullScreenWrapper>
       <input type="hidden" name="form-name" value="contact" />
-      {isSubmitted ? (
+      {/* {isSubmitted ? (
         <strong>Thanks for your feedback</strong>
-      ) : (
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          onSubmit={() => {
-            setIsSubmitted(true);
-          }}
-        >
-          <div className="flex flex-col">
-            <label htmlFor="name">Your Name:</label>
-            <Input type="text" name="name" id="name" />
+      ) : ( */}
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        onSubmit={() => {
+          setIsSubmitted(true);
+        }}
+      >
+        <div className="flex flex-col">
+          <label htmlFor="name">Your Name:</label>
+          <Input type="text" name="name" id="name" />
 
-            <label htmlFor="email">Your Email:</label>
-            <Input type="text" name="email" id="email" />
+          <label htmlFor="email">Your Email:</label>
+          <Input type="text" name="email" id="email" />
 
-            <label htmlFor="message">Feedback:</label>
-            <TextArea name="message" id="message"></TextArea>
+          <label htmlFor="message">Feedback:</label>
+          <TextArea name="message" id="message"></TextArea>
 
-            <Button type="submit">Send</Button>
-          </div>
-        </form>
-      )}
+          <Button type="submit">Send</Button>
+        </div>
+      </form>
+      {/* )} */}
     </FullScreenWrapper>
   );
 };
